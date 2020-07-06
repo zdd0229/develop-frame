a5a364f4a48df4c8dd548f4bc9ad8e02169b0b62
Z <1942209364@qq,com>
2020-07-06 22:58:43 +0800

    jar 引入

---
+++ src/main/java/com/z/DevelopFrameApplication.java
@@ -0,0 +1,11 @@
+package com.z;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopFrameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopFrameApplication.class,args);
+    }
+}
